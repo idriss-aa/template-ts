38add5d6fc9f6e59cfae00a2c6eba5af5a01f014
iaazza <idriss.aazza@capgemini.com>
2025-02-27 14:40:00 +0100

    add uml diagram

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import './index.css';
-import { watchModel } from './watch/watchModel';
-import { watchView } from './watch/watchView';
 import { watchController } from './watch/watchController';
 
 document.addEventListener('DOMContentLoaded', () => {
    new watchController(1);
-   new watchController(2);
-   new watchController(3);
 });
